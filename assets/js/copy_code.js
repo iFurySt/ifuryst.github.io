509ad63bba490581133c26dc3bae9c7506ec5a1d
Leo <ifuryst@gmail.com>
2026-05-17 20:21:25 +0800

    Polish code block and table styles

--- assets/js/copy_code.js
+++ assets/js/copy_code.js
@@ -1,4 +1,14 @@
 // create element for copy button in code blocks
+var copyIcon =
+  '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16">' +
+  '<path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>' +
+  '<path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>' +
+  "</svg>";
+var checkIcon =
+  '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16">' +
+  '<path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>' +
+  "</svg>";
+
 var codeBlocks = document.querySelectorAll("pre");
 codeBlocks.forEach(function (codeBlock) {
   if (
@@ -15,8 +25,8 @@ codeBlocks.forEach(function (codeBlock) {
     copyButton.className = "copy";
     copyButton.type = "button";
     copyButton.ariaLabel = "Copy code to clipboard";
-    copyButton.innerText = "Copy";
-    copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i>';
+    copyButton.title = "Copy code";
+    copyButton.innerHTML = copyIcon;
 
     // get code from code block and copy to clipboard
     copyButton.addEventListener("click", function () {
@@ -32,13 +42,17 @@ codeBlocks.forEach(function (codeBlock) {
         var code = codeBlock.querySelector("code").innerText.trim();
       }
       window.navigator.clipboard.writeText(code);
-      copyButton.innerText = "Copied";
-      copyButton.innerHTML = '<i class="fa-solid fa-clipboard-check"></i>';
+      copyButton.classList.add("copied");
+      copyButton.ariaLabel = "Copied";
+      copyButton.title = "Copied";
+      copyButton.innerHTML = checkIcon;
       var waitFor = 3000;
 
       setTimeout(function () {
-        copyButton.innerText = "Copy";
-        copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i>';
+        copyButton.classList.remove("copied");
+        copyButton.ariaLabel = "Copy code to clipboard";
+        copyButton.title = "Copy code";
+        copyButton.innerHTML = copyIcon;
       }, waitFor);
     });
 
